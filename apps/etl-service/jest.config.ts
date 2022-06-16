ff205754dda2bf041f43ef4dc1f00f58af312957
Alon Ben Haim <alon.b.h.672@gmail.com>
2022-06-16 17:51:43 +0300

    initial commit

---
+++ apps/etl-service/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'etl-service',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/etl-service',
+};
